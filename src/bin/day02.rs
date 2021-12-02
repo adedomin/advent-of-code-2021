3d0b6119ddce921ba84df0b5d0e8b8967b5ea718
Anthony DeDominic <adedomin@gmail.com>
2021-12-02 17:36:01 -0500

    fix underflow on weird inputs, make it 8ms faster on large input.

--- src/bin/day02.rs
+++ src/bin/day02.rs
@@ -1,24 +1,32 @@
 use std::{env, fs, io};
 
-fn parse_and_solve(input: String) -> (u64, u64) {
-    let (_, hor, aim, dep) =
-        input
-            .split_ascii_whitespace()
-            .fold((b'f', 0, 0, 0), |(dir, hor, aim, dep), tok| {
-                let pre = *tok.as_bytes().get(0).unwrap();
-                match pre {
-                    b'f' | b'd' | b'u' => (pre, hor, aim, dep),
-                    _ => {
-                        let vec = tok.parse::<u64>().unwrap();
-                        match dir {
-                            b'f' => (dir, hor + vec, aim, dep + vec * aim),
-                            b'd' => (dir, hor, aim + vec, dep),
-                            b'u' => (dir, hor, aim - vec, dep),
-                            _ => unreachable!(),
-                        }
+// we don't care about invalid input, yolo
+fn ascii_str_to_u64(input: &[u8]) -> i64 {
+    input
+        .iter()
+        .take(20) // digits bigger than this should not exist (u64)
+        .fold(0, |a, c| a * 10 + (c - b'0') as i64)
+}
+
+fn parse_and_solve(input: Vec<u8>) -> (i64, i64) {
+    let (_, hor, aim, dep) = input
+        .split(|&chr| chr == b' ' || chr == b'\n') /* perf says this is the devil */
+        .filter(|&l| !l.is_empty())
+        .fold((b'f', 0, 0, 0), |(dir, hor, aim, dep), tok| {
+            let pre = unsafe { *tok.get_unchecked(0) };
+            match pre {
+                b'f' | b'd' | b'u' => (pre, hor, aim, dep),
+                _ => {
+                    let vec = ascii_str_to_u64(tok);
+                    match dir {
+                        b'f' => (dir, hor + vec, aim, dep + vec * aim),
+                        b'd' => (dir, hor, aim + vec, dep),
+                        b'u' => (dir, hor, aim - vec, dep),
+                        _ => unreachable!(),
                     }
                 }
-            });
+            }
+        });
     (hor * aim /* part 1 */, hor * dep /* part 2 */)
 }
 
@@ -26,7 +34,7 @@ pub fn main() -> io::Result<()> {
     let mut args = env::args();
     args.next();
     let filename = args.next().unwrap_or_else(|| "/dev/stdin".to_owned());
-    let input = fs::read_to_string(filename)?;
+    let input = fs::read(filename)?;
     let (p1, p2) = parse_and_solve(input);
     println!("Part1 {}, Part2 {}", p1, p2);
     Ok(())
